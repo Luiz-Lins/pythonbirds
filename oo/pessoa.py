431f7257ddf7d15b43ecdca1d90b232fea2a88a4
lhsilva <luizlins22@gmail.com>
2022-06-18 15:47:54 -0300

    Criados atribudos de instância nome e idade.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,14 @@
 class Pessoa:
+    def __init__(self, nome=None):
+        self.nome = None
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
     p = Pessoa()
     print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Luiz'
+    print(p.nome)
