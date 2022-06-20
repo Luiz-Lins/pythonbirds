cfb19cc35a1a1409a7fcab3064d6802460cde26b
lhsilva <luizlins22@gmail.com>
2022-06-20 00:03:35 -0300

    Implementado Class carro

--- oo/carro.py
+++ oo/carro.py
@@ -92,6 +92,25 @@ Exemplo:
     >>> carro.girar_a_esquerda()
     >>> carro.calcular_direcao()
     'Oeste'"""
+
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+    def acelerar(self):
+        self.motor.acelerar()
+    def frear(self):
+        self.motor.frear()
+    def calcular_direcao(self):
+        return self.direcao.valor
+    def girar_a_direita(self):
+        return self.direcao.girar_a_direita()
+    def girar_a_esquerda(self):
+        return self.direcao.girar_a_esquerda()
+
 NORTE = 'Norte'
 SUL = 'Sul'
 LESTE = 'Leste'
